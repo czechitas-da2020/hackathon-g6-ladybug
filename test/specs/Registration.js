7c0bd020bb6105a253f3f691040856b6e9a51d6d
Miriama Svobodova <miriama.svobodova@gmail.com>
2021-05-22 17:59:47 +0200

    New user registration

---
+++ test/specs/Registration.js
@@ -0,0 +1,33 @@
+describe('Registration and login', () => {
+    it('registration new user', () => {
+        browser.url('http://czechitas-datestovani-hackathon.cz/en');
+        browser.pause(1000);
+        browser.maximizeWindow()
+        browser.pause(1000);
+        browser.$(".hide_xs").click()
+        browser.pause(1000);
+        
+        let useremail = ('franta.liska+'+ Math.floor(Math.random()*100000)+1 +'@gmail.com')
+
+        browser.$('#email_create').setValue(useremail) 
+        browser.pause(1000);
+        console.log(useremail)
+        browser.$('.icon-user').click()
+        browser.pause(1000);
+
+        let firstname = 'Franta'
+        let lastname = "Liška"
+        let password = 'hackathon'
+
+        browser.$('#customer_firstname').setValue(firstname) 
+        browser.$('#customer_lastname').setValue(lastname) 
+        browser.$('#passwd').setValue(password)
+        browser.$('#submitAccount').scrollIntoView()
+        console.log(password)
+        browser.$('#submitAccount').click()
+        browser.pause(1000);
+       
+        expect($('h1=My account')).toBeDisplayedInViewport()
+    });
+
+   });
