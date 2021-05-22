6ba5f09deb6adb0707fe92a4658f39eaec6286f2
Miriama Svobodova <miriama.svobodova@gmail.com>
2021-05-22 18:13:30 +0200

    login

--- test/specs/Loginauto.js
+++ test/specs/Loginauto.js
@@ -1,18 +1,67 @@
-describe('Feature LOGIN', () => {
-    // TEST-CASE - jakym zpusobem to budeme testovat
-    it('is possible to log into app with correct credentials', () => {
-        let username = 'czechitas'
-        let password = 'D4Ostr4v42020'
- 
-        // KROKY PRO OVERENI 
-        browser.url('https://automation.cervik.repl.co/login.html');
-        $('#login-username').setValue(username) // VYPLNIM JMENO
-        $('#login-password').setValue(password) // VYPLNIM HESLO
-        $('#login-submit').click()              // KLIKNU NA TLACITKO "LOG IN!"
- 
-        $('p*=LOGGED!').waitForDisplayed()
- 
-        // OVERENI - ASSERTION
-        expect($('h1=LOGGED!')).toBeDisplayedInViewport()
-    });
-});
+// describe('Login as new registered user', () => {
+//     it('registration new user', () => {
+//         browser.url('http://czechitas-datestovani-hackathon.cz/en');
+//         browser.pause(1000);
+//         browser.maximizeWindow()
+//         browser.pause(1000);
+//         browser.$(".hide_xs").click()
+//         browser.pause(1000);
+        
+//         let useremail = ('franta.liska+'+ Math.floor(Math.random()*100000)+1 +'@gmail.com')
+
+//         browser.$('#email_create').setValue(useremail) 
+//         browser.pause(1000);
+//         console.log(useremail)
+//         browser.$('.icon-user').click()
+//         browser.pause(1000);
+
+//         let firstname = 'Franta'
+//         let lastname = "Liška"
+//         let password = 'hackathon'
+
+//         browser.$('#customer_firstname').setValue(firstname) 
+//         browser.$('#customer_lastname').setValue(lastname) 
+//         browser.$('#passwd').setValue(password)
+//         browser.$('#submitAccount').scrollIntoView()
+//         console.log(password)
+//         browser.$('#submitAccount').click()
+//         browser.pause(1000);
+       
+//         expect($('h1=My account')).toBeDisplayedInViewport()
+//         browser.$('.caret').click()
+//         browser.$(a[href="http://czechitas-datestovani-hackathon.cz/en/?mylogout="].click()
+        
+//     );
+
+//     // it('Login as new registered user', () => {
+//     //     browser.url('http://czechitas-datestovani-hackathon.cz/en');
+//     //     browser.pause(1000);
+//     //     browser.maximizeWindow()
+//     //     browser.pause(1000);
+//     //     browser.$(".hide_xs").click()
+//     //     browser.pause(1000);
+        
+//     //     let useremail = ('franta.liska+'+ Math.floor(Math.random()*100000)+1 +'@gmail.com')
+
+//     //     browser.$('#email_create').setValue(useremail) 
+//     //     browser.pause(1000);
+//     //     console.log(useremail)
+//     //     browser.$('.icon-user').click()
+//     //     browser.pause(1000);
+
+//     //     let firstname = 'Franta'
+//     //     let lastname = "Liška"
+//     //     let password = 'hackathon'
+
+//     //     browser.$('#customer_firstname').setValue(firstname) 
+//     //     browser.$('#customer_lastname').setValue(lastname) 
+//     //     browser.$('#passwd').setValue(password)
+//     //     browser.$('#submitAccount').scrollIntoView()
+//     //     console.log(password)
+//     //     browser.$('#submitAccount').click()
+//     //     browser.pause(1000);
+       
+//     //     expect($('h1=My account')).toBeDisplayedInViewport()
+//     // });
+
+//    });
